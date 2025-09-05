186cbbc9f32bc680bb47e4b4333d594184d0c66c
Alexis Barragam <barragamalexis@gmail.com>
2025-09-05 07:53:17 -0300

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -1,22 +1,19 @@
 # load_data.py
 from sqlalchemy import create_engine
-# Importa a função de limpeza do outro arquivo
 from explore_data import clean_student_data, FILE_PATH
 
-# --- Suas informações de conexão ---
+
 DB_USER = 'postgres'
 DB_PASSWORD = '05032016'
 DB_HOST = 'localhost'
 DB_PORT = '1612'
 DB_NAME = 'students_mental_health'
-# ---------------------------------
+
 
 CONNECTION_STRING = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
 def load_clean_data_to_postgres():
-    """
-    Roda a limpeza dos dados e carrega o DataFrame limpo para o PostgreSQL.
-    """
+    
     print("Running data cleaning and preparation script...")
     clean_df = clean_student_data(FILE_PATH)
 
@@ -25,7 +22,7 @@ def load_clean_data_to_postgres():
             engine = create_engine(CONNECTION_STRING)
             print("\nConnecting to PostgreSQL to load data...")
 
-            # Carrega o DataFrame JÁ LIMPO para a tabela 'students'
+            
             clean_df.to_sql('students', engine, if_exists='replace', index=False)
             
             print("\nSUCCESS: Clean data was loaded into your database!")
